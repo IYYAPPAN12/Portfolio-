7b84b6c472426c9a9320a74c8e51f988ae2d1573
IYYAPPAN12 <iyappakeerthi22@gmail.com>
2023-11-07 12:32:48 +0530

    modified

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 $(document).ready(function () {
     $(window).scroll(function () {
-      //  sticky navbar on scroll script  //
       if (this.scrollY > 20) {
         $(".navbar").addClass("sticky");
       } else {
         $(".navbar").removeClass("sticky");
       }
   
-      //  scroll-up button show/hide script  //
       if (this.scrollY > 500) {
         $(".scroll-up-btn").addClass("show");
       } else {
@@ -15,28 +13,23 @@ $(document).ready(function () {
       }
     });
   
-    //  slide-up script  //
   
     $(".scroll-up-btn").click(function () {
       $("html").animate({ scrollTop: 0 });
-      //  removing smooth scroll on slide-up button click  //
       $("html").css("scrollBehavior", "auto");
     });
   
     $(".navbar .menu li a").click(function () {
-      //  Smooth scroll on Menu Items click  //
   
       $("html").css("scrollBehavior", "smooth");
     });
   
-    //  Toggle Navbar  //
   
     $(".menu-btn").click(function () {
       $(".navbar .menu").toggleClass("active");
       $(".menu-btn i").toggleClass("active");
     });
   
-    //  Typing Text Animation  //
   
     var typed = new Typed(".typing", {
       strings: [
@@ -63,7 +56,6 @@ $(document).ready(function () {
       loop: true
     });
   
-    //  Owl Carousel  //
   
     $(".carousel").owlCarousel({
       margin: 20,
